f193bf07a0ccea408eab8363602de6ba82ff78d3
Daniel Banariba <banaribad@gmail.com>
2024-10-15 02:43:29 -0600

    Sistema Contable, face 1

---
+++ main.tf
@@ -0,0 +1,4 @@
+provider "azurerm" { 
+    features {} # Configuraciones globales
+    subscription_id = var.subscription_id # ID de suscripción de Azure
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "subscription_id" {
+    description = "ID de suscripción de Azure"
+    type        = string
+}
+
